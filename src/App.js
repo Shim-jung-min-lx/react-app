40f89f5ec4dc18e805f6fc12cd0f868a9efd82e7
SJM <simjung418@gmail.com>
2023-12-10 17:52:51 +0900

    4,5강 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+function Header(props) {
+  return (
+    <header>
+      <h1><a href="/">{props.title}</a></h1>
+    </header>
+  )
+}
+function Nav(props) {
+  console.log(props)
+  const list = []
+  for(let i=0; i<props.topics.length; i++) {
+    let t = props.topics[i]
+    list.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+  }
+  return (
+    <nav>
+      <ol>
+        {list}
+      </ol>
+    </nav>
+  )
+}
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  )
+}
 
 function App() {
+  const topics = [
+    {id:1, title:'html5', body: 'html5 is ...'},
+    {id:2, title:'css3', body: 'css3 is ...'},
+    {id:3, title:'javascript', body: 'javascript is ...'}
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title='React'></Header>
+      <Nav topics={topics}></Nav>{/* 따옴표 없이 중괄호로 감싸면 변수로 지정 가능 */}
+      <Article title="Hi, stranger" body="hello, website"></Article>
     </div>
-  );
+  )
 }
 
 export default App;
